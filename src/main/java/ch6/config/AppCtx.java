132ccfd4af0a7b3cff8067cf7d4361a8cfd5f1b3
Seongwon97 <osw970325@naver.com>
2022-05-05 17:34:34 +0900

    feat: chapter6 내용 정리 & 예제 코드 작성

--- src/main/java/ch6/config/AppCtx.java
+++ src/main/java/ch6/config/AppCtx.java
@@ -1,5 +1,7 @@
 package ch6.config;
 
+import ch6.domain.Client;
+import ch6.domain.Client2;
 import ch6.printer.MemberPrinter;
 import ch6.printer.MemberSummaryPrinter;
 import ch6.printer.VersionPrinter;
@@ -7,9 +9,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackages = {"ch5"})
+@ComponentScan(basePackages = {"ch6"})
 public class AppCtx {
 
     @Bean
@@ -30,5 +33,20 @@ public class AppCtx {
         versionPrinter.setMajorVersion(5);
         versionPrinter.setMinorVersion(0);
         return versionPrinter;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Client client() {
+        Client client = new Client();
+        client.setHost("host");
+        return client;
+    }
+
+    @Bean(initMethod = "connect", destroyMethod = "close")
+    public Client2 client2() {
+        Client2 client2 = new Client2();
+        client2.setHost("host");
+        return client2;
     }
 }
---
+++ src/main/java/ch6/domain/Client2.java
@@ -0,0 +1,22 @@
+package ch6.domain;
+
+public class Client2 {
+
+    private String host;
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void connect() {
+        System.out.println("Client2.connect() 실행");
+    }
+
+    public void send() {
+        System.out.println("Client2.send() 실행");
+    }
+
+    public void close() {
+        System.out.println("Client2.close() 실행");
+    }
+}
